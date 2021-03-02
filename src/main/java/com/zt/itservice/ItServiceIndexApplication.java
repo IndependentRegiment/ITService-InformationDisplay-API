937b7ebcf88cf3a0ebbb485bd6b990d5457a9a52
黄浙浩 <huangzhehaocool@163.com>
2021-03-02 10:16:33 +0800

    对项目进行版本管理

---
+++ src/main/java/com/zt/itservice/ItServiceIndexApplication.java
@@ -0,0 +1,13 @@
+package com.zt.itservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItServiceIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItServiceIndexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zt/itservice/ItServiceIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zt.itservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItServiceIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
